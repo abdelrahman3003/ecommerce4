84c9713d0e421550e0a873630fd365ce078b1a49
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-03 19:40:01 +0200

    create onbooarding model

---
+++ lib/data/model/oncoarding_model.dart
@@ -0,0 +1,8 @@
+class OnboradingModel {
+  final String title;
+  final String subtitle;
+  final String image;
+
+  OnboradingModel(
+      {required this.title, required this.subtitle, required this.image});
+}
