d52708bf6ead24b3c59ad100bec61718847b7737
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-03 17:38:15 +0200

    text1

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class Ecommerce extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      color: Colors.yellow,
     );
   }
 }
