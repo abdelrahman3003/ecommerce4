cf77b98fe9632e9ab7bd4fba785f61aac2b6fbcc
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-05 09:19:44 +0200

    problem solving

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:eccommerce4/core/constant/routsApp.dart';
+import 'package:eccommerce4/core/services/services.dart';
 import 'package:eccommerce4/view/onboarding/onboarding.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await inialServices();
   runApp(const Ecommerce());
 }
 
@@ -13,7 +16,6 @@ class Ecommerce extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      //translations: AppTrnaslations(),
       home: const Onboarding(),
       debugShowCheckedModeBanner: false,
       routes: routes,
