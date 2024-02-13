d84a89982fd0a4aad826a4b1839f0d9cbb1f3acd
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-13 10:38:23 +0200

    problem solving in server host

--- lib/core/constant/backlinks.dart
+++ lib/core/constant/backlinks.dart
@@ -1,2 +1,2 @@
-const String serverNameLink = "https://temsah.atwebpages.com/ecommerce";
+const String serverNameLink = "http://temsah.atwebpages.com/ecommerce/";
 const String signupNameLink = "$serverNameLink/auth/signup.php";
