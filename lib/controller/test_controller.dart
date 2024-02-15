24335a9d42b258da851c50ccd9476cc7ae1c3d37
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-15 06:31:59 +0200

    test controller finshed

--- lib/controller/test_controller.dart
+++ lib/controller/test_controller.dart
@@ -1,3 +1,25 @@
+import 'package:eccommerce4/core/class/statuscode.dart';
+import 'package:eccommerce4/core/functions/handling%20_data.dart';
+import 'package:eccommerce4/view/test.dart';
 import 'package:get/get.dart';
 
-class TestController extends GetxController {}
+class TestController extends GetxController {
+  TestData testData = TestData(Get.find());
+  late StatusRequest statusRequest;
+  List data = [];
+  getData() async {
+    statusRequest = StatusRequest.loading;
+    var response = testData.getData();
+    statusRequest = handlingApiData(response);
+    if (statusRequest == StatusRequest.success) {
+      data.addAll(response['data']);
+    }
+    update();
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    getData();
+  }
+}
