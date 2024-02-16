19b5a5f106ee623e4604b5e4550cc956c5e0de31
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-16 18:12:35 +0200

    signin controller

---
+++ lib/controller/auth/signin_controller.dart
@@ -0,0 +1,30 @@
+import 'package:eccommerce4/core/constant/routsApp.dart';
+import 'package:get/get.dart';
+
+import '../../core/class/statuscode.dart';
+import '../../core/functions/handling _data.dart';
+import '../../data/datasource/remote/posdata_signin.dart';
+
+class SigninController extends GetxController {
+  PostDataSignin postDataSignup = PostDataSignin(Get.find());
+  StatusRequest? statusRequest;
+
+  signin(String email, String password) async {
+    statusRequest = StatusRequest.loading;
+    update();
+    var response = await postDataSignup.postDataSignup(email, password);
+    statusRequest = handlingApiData(response);
+    if (statusRequest == StatusRequest.success) {
+      if (response["status"] == "failure") {
+        statusRequest = StatusRequest.failure;
+        Get.defaultDialog(
+          title: "error",
+          middleText: "invalid details",
+        );
+      } else {
+        Get.toNamed(kSignupSucess);
+      }
+      update();
+    } else {}
+  }
+}
