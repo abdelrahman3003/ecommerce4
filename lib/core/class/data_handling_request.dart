c7bb7637d21c7a8d4d0cb07fe303bd50bf5fb95b
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-17 16:59:12 +0200

    create data handling for only rquset no for data show or get data from api

---
+++ lib/core/class/data_handling_request.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/widgets.dart';
+import '../constant/lotties/Offine_lottie.dart';
+import '../constant/lotties/error404_lottie.dart';
+import '../constant/lotties/loading_lottie.dart';
+import 'statuscode.dart';
+
+class DataHandlingRequsetState extends StatelessWidget {
+  const DataHandlingRequsetState(
+      {super.key, required this.statusRequest, required this.widget});
+  final StatusRequest statusRequest;
+  final Widget widget;
+  @override
+  Widget build(BuildContext context) {
+    return statusRequest == StatusRequest.loading
+        ? const LoadingLottie()
+        : statusRequest == StatusRequest.offlineFailure
+            ? const Offline()
+            : statusRequest == StatusRequest.serverFailure
+                ? const Error404()
+                : widget;
+  }
+}
