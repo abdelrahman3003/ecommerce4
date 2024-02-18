8aeef07779e7f4c7444e1111c42e4e2ca773bc96
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-18 17:02:36 +0200

    custom appbar

--- lib/view/home/widget/home_appbar.dart
+++ lib/view/home/widget/home_appbar.dart
@@ -5,6 +5,22 @@ class HomeAppbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 60,
+      width: double.infinity,
+      decoration:
+          const BoxDecoration(color: Color.fromARGB(255, 236, 229, 229)),
+      child: Row(
+        children: [
+          const SizedBox(width: 20),
+          const Icon(Icons.search, size: 28),
+          const SizedBox(width: 20),
+          Expanded(child: TextFormField()),
+          const SizedBox(width: 20),
+          const Icon(Icons.notifications_active_outlined, size: 28),
+          const SizedBox(width: 20),
+        ],
+      ),
+    );
   }
 }
