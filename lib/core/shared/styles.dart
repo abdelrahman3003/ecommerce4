df778affe523bdb304ef25c4f0e5b3c573f7c0e8
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-18 19:09:56 +0200

    ui inhance

--- lib/core/shared/styles.dart
+++ lib/core/shared/styles.dart
@@ -4,10 +4,10 @@ import 'package:flutter/material.dart';
 abstract class Styles {
   static const textStyle18 = TextStyle(
     fontSize: 18,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.normal,
   );
-  static const textStyle20 = TextStyle(
-      fontSize: 20, fontWeight: FontWeight.normal, color: Colors.white);
+  static const textStyle20 =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white);
   static const textStyle25 = TextStyle(
     fontSize: 25,
     fontWeight: FontWeight.normal,
