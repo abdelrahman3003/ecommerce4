089c5487e29ad306aecbcad5712d39e98e58c68e
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-18 09:10:50 +0200

    problem solving in firebase version

--- lib/core/services/services.dart
+++ lib/core/services/services.dart
@@ -1,3 +1,4 @@
+import 'package:eccommerce4/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -5,7 +6,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AppServices extends GetxService {
   late SharedPreferences sharedPreferences;
   Future<AppServices> init() async {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
     sharedPreferences = await SharedPreferences.getInstance();
     return this;
   }
