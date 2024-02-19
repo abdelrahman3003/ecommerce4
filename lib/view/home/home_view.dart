b0db94429ae0d6b25e26fedcd9ff7b6a5bd5fdb8
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-19 06:11:13 +0200

    move folder

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'widget/home_view_body.dart';
+import 'home_page_view/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
