830693b259ac7b62832bfd0166258dcefb6a1b0b
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-22 10:40:15 +0200

    remove folder

--- lib/view/home/items_view.dart
+++ lib/view/home/items_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'items_view/items_view_body.dart';
+import '../items/items_view_body.dart';
 
 class ItemsView extends StatelessWidget {
   const ItemsView({super.key});
