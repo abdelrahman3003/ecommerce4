05307d95dc0cd293c872b79a5902a3569f98604e
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-23 15:20:10 +0200

    favourite controller

---
+++ lib/controller/items/favourite_controller.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+
+abstract class FavouriteController extends GetxController {
+  setFavourite(String id, String val);
+}
+
+class FavouriteControllerImp extends FavouriteController {
+  Map favourite = {};
+
+  @override
+  setFavourite(id, val) {
+    favourite[id] = val;
+  }
+}
