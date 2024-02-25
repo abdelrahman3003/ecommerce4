dc9ba734681c578a2fa8482705600840a7fff844
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-25 09:45:35 +0200

    custom appabr

---
+++ lib/view/home/widget/cart/appbar_cart.dart
@@ -0,0 +1,33 @@
+import 'package:eccommerce4/core/shared/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AppbarCart extends StatelessWidget {
+  const AppbarCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 70,
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_back,
+              size: 30,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: Get.width / 3.5),
+            child: Text(
+              "My Cart",
+              style: Styles.textStyle25
+                  .copyWith(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
