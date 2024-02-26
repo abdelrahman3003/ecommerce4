995bfebb5761949005fb2a66feb1fa7af1beaf12
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-26 10:36:49 +0200

    refactor code

--- lib/view/items/widget/item_details/item_price.dart
+++ lib/view/items/widget/item_details/item_price.dart
@@ -4,64 +4,67 @@ import 'package:eccommerce4/core/shared/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class ItemPrice extends GetView<ItemsDetailsControllerImp> {
+class ItemPrice extends StatelessWidget {
   const ItemPrice({super.key, required this.price});
   final String price;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Row(
-            children: [
-              IconButton(
-                onPressed: () {
-                  controller.cartControllerImp
-                      .addCart(controller.itemModel.itemsId);
-                  controller.cartControllerImp.count++;
-                },
-                icon: const Icon(
-                  Icons.add,
-                  color: Colors.black,
-                ),
-              ),
-              Opacity(
-                opacity: .7,
-                child: Container(
-                  height: 40,
-                  width: 60,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(
-                      color: ColorsApp.kprimaryColor2,
-                      width: 3,
-                    ),
+    return GetBuilder<ItemsDetailsControllerImp>(
+        builder: (controller) => Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          controller.cartControllerImp
+                              .addCart(controller.itemModel.itemsId);
+                          controller.cartControllerImp.count++;
+                        },
+                        icon: const Icon(
+                          Icons.add,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Opacity(
+                        opacity: .7,
+                        child: Container(
+                          height: 40,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: ColorsApp.kprimaryColor2,
+                              width: 3,
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "${controller.cartControllerImp.count}",
+                              style: Styles.textStyle18,
+                            ),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          controller.cartControllerImp
+                              .removeCart(controller.itemModel.itemsId);
+                          controller.cartControllerImp.count++;
+                        },
+                        icon: const Icon(
+                          Icons.remove,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
                   ),
-                  child: const Text(
-                    "3",
-                    style: Styles.textStyle18,
-                  ),
                 ),
-              ),
-              IconButton(
-                onPressed: () {
-                  controller.cartControllerImp
-                      .removeCart(controller.itemModel.itemsId);
-                  controller.cartControllerImp.count++;
-                },
-                icon: const Icon(
-                  Icons.remove,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-          ),
-        ),
-        Text(
-          "\$ $price",
-          style: Styles.textStyle25.copyWith(color: Colors.red),
-        )
-      ],
-    );
+                Text(
+                  "\$ $price",
+                  style: Styles.textStyle25.copyWith(color: Colors.red),
+                )
+              ],
+            ));
   }
 }
