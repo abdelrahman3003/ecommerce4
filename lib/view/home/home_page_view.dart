ed715ad9401b8725ed5f5b32b398afb7ff83b1ec
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-02-28 20:47:44 +0200

    refactor code

--- lib/view/home/home_page_view.dart
+++ lib/view/home/home_page_view.dart
@@ -1,4 +1,7 @@
 import 'package:eccommerce4/controller/home/home_controller.dart';
+import 'package:eccommerce4/controller/search/search_controller.dart';
+import 'package:eccommerce4/view/home/widget/home_page_view/home_appbar.dart';
+import 'package:eccommerce4/view/search/serch_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,7 +13,20 @@ class HomePageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Get.put(HomePageControllerImp());
-    return const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 12), child: HomeViewBody());
+    Get.put(SearchControllerImp());
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const HomeAppbar(),
+              GetBuilder<SearchControllerImp>(
+                builder: (controller) => !controller.isSearch
+                    ? const HomeViewBody()
+                    : const SearchView(),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/view/home/widget/home_page_view/home_view_body.dart
+++ lib/view/home/widget/home_page_view/home_view_body.dart
@@ -1,12 +1,7 @@
-import 'package:eccommerce4/controller/search/search_controller.dart';
-import 'package:eccommerce4/view/search/serch_view.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
 import 'categories_listview.dart';
 import 'discount_container.dart';
-import 'home_appbar.dart';
 import 'producr_listview.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -14,29 +9,17 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Get.put(SearchControllerImp());
-    return Column(
+    return const Column(
       children: [
-        const HomeAppbar(),
-        GetBuilder<SearchControllerImp>(
-          builder: (controller) => controller.isSearch
-              ? const SingleChildScrollView(
-                  physics: BouncingScrollPhysics(),
-                  child: Column(
-                    children: [
-                      SizedBox(height: 20),
-                      DiscountContainer(),
-                      SizedBox(height: 20),
-                      CategoriesListView(),
-                      SizedBox(height: 20),
-                      ProductListView(text: "Product For Your"),
-                      SizedBox(height: 20),
-                      ProductListView(text: "Offer"),
-                      SizedBox(height: 50),
-                    ],
-                  ))
-              : const SearchView(),
-        )
+        SizedBox(height: 20),
+        DiscountContainer(),
+        SizedBox(height: 20),
+        CategoriesListView(),
+        SizedBox(height: 20),
+        ProductListView(text: "Product For Your"),
+        SizedBox(height: 20),
+        ProductListView(text: "Offer"),
+        SizedBox(height: 50),
       ],
     );
   }
